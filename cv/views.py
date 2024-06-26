e90bf7840fb9541a03ad324e18e7f8b2a0d14887
Fred <147978369+Phr7d@users.noreply.github.com>
2024-06-26 23:19:20 +0330

    Update some changes
    add view render that page load from datas.

--- cv/views.py
+++ cv/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 # Create your views here.
 
+context = {'name': 'Fred Shahin', 'birth':'17 July 1992', 'website':'https://www.phr7d.ir', 'Phone':'+98 912 408 8462', 'email':'m.aminshahin@gmail.com', 'Language':'Persian, English','Location':'No. 66, Molasadra, Vanak, Tehran, Iran'}
 def home(request):
     return render(request,'cv/index.html')
 
 def about(request):
-    return render(request,'cv/about.html')
+    return render(request,'cv/about.html',context)
 
 def resume(request):
     return render(request,'cv/resume.html')
@@ -17,7 +18,7 @@ def portfolio(request):
     return render(request,'cv/portfolio.html')
 
 def contact(request):
-    return render(request,'cv/contact.html')
+    return render(request,'cv/contact.html',context)
 
 def portfoliodetails(request):
     return render(request,'cv/pd.html')
